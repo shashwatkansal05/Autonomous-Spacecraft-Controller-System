5488dc387b281527e76b12f9bc5efc3332841b4b
Shashwat Kansal <shashwatkansal05@gmail.com>
2026-05-19 12:29:42 +0530

    Create FLAGS.h

---
+++ FLAGS.h
@@ -0,0 +1,11 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+#define TEMP_SENSOR_OK (1<<0)
+#define DIST_SENSOR_OK (1<<1)
+#define SYSTEM_SAFE (1<<2)
+#define COMM_ERROR (1<<3)
+#define SENSOR_TIMEOUT (1<<4)
+
+#endif
+
